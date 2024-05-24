b82143b660d5af3b2846bc7eb8eb0e692e71b636
Cauã Carvalho <caua2006carvalho@gmail.com>
2024-05-24 17:06:46 +0000

    feat: initial ccommit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import React from "react";
 import { Card } from "@/components/ui/Card";
-import Image from "next/image";
+import { Spotlight } from "@/components/ui/Spotlight";
 
 export default function Home() {
   return (
     <main className="bg-[#141414] h-svh flex justify-center items-center">
+      <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
       <Card/>
     </main>
   );
--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -9,12 +9,12 @@ export function Card() {
     const networks = ['Github', 'Frontend Mentor','Linkedln','Twitter','Instagram']
   return (
     <CardContainer className="inter-var">
-      <CardBody className="bg-[#1f1f1f] relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-20 sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+      <CardBody className="bg-[#1f1f1f] relative group/card flex  flex-col items-center  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-20 sm:w-[20rem] h-auto rounded-xl p-6 border">
         <CardItem
           translateZ="50"
           className="text-xl font-bold text-white dark:text-white"
         >
-          Cauã Carvalho
+          Cauã Carvalho 
         </CardItem>
         <CardItem
           as="p"
@@ -23,16 +23,16 @@ export function Card() {
         >
           Los Angeles, USA
         </CardItem>
-        <CardItem translateZ="100" className="w-full mt-4">
+        <CardItem translateZ="100" className="w-full mt-4 flex justify-center">
           <Image
             src="/102489942.jfif"
-            height="200"
-            width="200"
-            className=" w-full object-cover rounded-full group-hover/card:shadow-xl"
+            height="150"
+            width="150"
+            className="size-32 object-cover rounded-full group-hover/card:shadow-xl"
             alt="thumbnail"
           />
         </CardItem>
-        <div className="flex flex-col items-center gap-3
+        <div className="flex flex-col items-center gap-3 w-full
           mt-20">
 
             {networks.map((network)=>  (
@@ -41,7 +41,7 @@ export function Card() {
                 as="button"
                 className="w-full px-4 py-2 bg-[#333333] rounded-xl  text-white text-xs font-bold hover:bg-[#c5f82a]"
               >
-                {network}
+                {network} 
               </CardItem>
             ))}
           
